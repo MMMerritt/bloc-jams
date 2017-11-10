17cdf3c3755ee8130f64984c87fabb24a55965bc
Michelle Merritt <merritt.michellem@gmail.com>
2017-11-09 23:36:05 -0800

    Assignement 21- Animations Complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,6 +2,18 @@ var animatePoints = function() {
 
         var points = document.getElementsByClassName('point');
 
+        var revealPoints = function(index) {
+            points[index].style.opacity = 1;
+            points[index].style.transform = "scaleX(1) translateY(0)";
+            points[index].style.msTransform = "scaleX(1) translateY(0)";
+            points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+        }
+
+        for(var i = 0; i < points.length; i++) {
+            revealPoints(i);
+        }
+
+        /*
         var revealFirstPoint = function() {
             points[0].style.opacity = 1;
             points[0].style.transform = "scaleX(1) translateY(0)";
@@ -23,8 +35,10 @@ var animatePoints = function() {
             points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
         };
 
+
         revealFirstPoint();
         revealSecondPoint();
         revealThirdPoint();
+        */
 
     };
