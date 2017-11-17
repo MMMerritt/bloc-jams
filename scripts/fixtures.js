8b4459591834ad7c8724584196928baf0fe16e05
Michelle Merritt <merritt.michellem@gmail.com>
2017-11-17 12:51:11 -0800

    Checkpoint 31 jQuery: Next and Previous Complete

---
+++ scripts/fixtures.js
@@ -0,0 +1,47 @@
+// Example Album
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+
+ // Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ // A Third Example Album
+ var albumKate = {
+     title: 'Hounds of Love',
+     artist: 'Kate Bush',
+     label: 'EMI Records',
+     year: '1985',
+     albumArtUrl: 'assets/images/album_covers/02.png',
+     songs: [
+         { title: 'Running Up That Hill (A Deal with God)', duration: '5:03' },
+         { title: 'Hounds of Love', duration: '3:02' },
+         { title: 'The Big Sky', duration: '4:41'},
+         { title: 'Mother Stands for Comfort', duration: '3:07' },
+         { title: 'Cloudbusting', duration: '5:10'}
+     ]
+ };
